17e1c4f94d8a817fdd5a8904fc28f202358d142a
Taha Abd El Sabour <tahaclr@outlook.com>
2020-05-19 11:44:54 +0200

    Birthdate Property

--- OurFinalProject/OurFinalProject/Models/Employee.cs
+++ OurFinalProject/OurFinalProject/Models/Employee.cs
@@ -10,5 +10,7 @@ namespace OurFinalProject.Models
         public int ID { get; set; }
         public string Name {get; set;}
         public decimal Salary {get;set;}
+
+        public DateTime BirthData { get; set; }
     }
 }
