98c3861cfd9e52ea6072729e176de08be26bcda4
ITILuxor <65046138+ITILuxor@users.noreply.github.com>
2020-05-19 11:44:04 +0200

    Update Employee.cs

--- OurFinalProject/OurFinalProject/Models/Employee.cs
+++ OurFinalProject/OurFinalProject/Models/Employee.cs
@@ -9,6 +9,6 @@ namespace OurFinalProject.Models
     {
         public int ID { get; set; }
         public string Name {get; set;}
-        public decimal Salary {get;set;}
+        public decimal EmpSalary {get;set;}
     }
 }
